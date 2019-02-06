2cfbaf6cb07bd7cdd6d894a5c4efffd08bdd19f1
Dan Shields <d.shieldsuk@gmail.com>
2019-02-06 20:08:57 +0000

    v0.2.0

--- src/components/NCGVueText.ts
+++ src/components/NCGVueText.ts
@@ -1,6 +1,20 @@
+import Vue from 'vue';
 import VTextField from "vuetify/es5/components/VTextField";
+import ReplicantComponent from "./Replicant"
+import { NCGVueInput } from '../types/NCGVueInput';
 
-export default {
+export default Vue.extend({
   name: "ncg-vue-text",
-  extends: VTextField
-};
+  extends: VTextField,
+  mixins: [ReplicantComponent],
+  methods: {
+    replicantChangeHandler(value: any) {
+      (this as unknown as NCGVueInput).value = value;
+    }
+  },
+  created() {
+    this.$on('change', (val: string) => {
+      (this as unknown as NCGVueInput).replicant.value = val
+    });
+  }
+});
