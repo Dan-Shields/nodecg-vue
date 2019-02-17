4050e27f9ead7453645cb7e14d0d30ef5f499519
Dan Shields <d.shieldsuk@gmail.com>
2019-02-17 18:46:57 +0000

    Remove retargeting for replicantOpts and strip down ReplicantTargeting

--- src/components/ReplicantTargeting.js
+++ src/components/ReplicantTargeting.js
@@ -3,54 +3,52 @@ import Vue from 'vue';
 export default Vue.extend({
     data() {
         return {
-            replicant: null
+            replicant: null,
+            replicantDeclared: false
         }
     },
     props: {
         replicantName: String,
         replicantBundle: { type: String, default: typeof window.nodecg === 'object' ? window.nodecg.bundleName : null },
-        replicantOpts: Object
+        replicantOpts: {type: Object, default: {}}
     },
     mounted() {
-        if (!this.replicant && this.replicantName && this.replicantBundle) {
+        if (!this.replicant && this.replicantName) {
             this.declareReplicant(this.replicantName, this.replicantBundle);
         }
     },
     methods: {
-        declareReplicant(name, bundle) {
+        declareReplicant(name, bundle = this.bundleName) {
             if (!name || !bundle) {
                 return;
             }
-            const opts = {
-                schemaPath: `bundles/${encodeURIComponent(bundle)}/schemas/${encodeURIComponent(name)}.json`
-            };
-            this.replicant = nodecg.Replicant(name, bundle, opts);
-            this.replicant.on('change', this._replicantChangeHandler);
 
-            this.$emit('retarget', {name, bundle});
-        },
-        retargetReplicant(name = this.replicantName, bundle = this.replicantBundle) {
-            if (!name || !bundle) {
-                return;
-            }
             // If there is an existing replicant, remove the event listener
             if (this.replicant) {
                 this.replicant.removeListener('change', this._replicantChangeHandler);
             }
-            this.declareReplicant(name, bundle);
+
+            this.replicant = nodecg.Replicant(name, bundle, this.replicantOpts);
+            this.replicant.on('change', this._replicantChangeHandler);
+            
+            this.$emit('retarget', {name, bundle});
         },
         _replicantChangeHandler(...args) {
+            this.replicantDeclared = true;
+
+            this.$emit('change', args);
+
             if (typeof this.replicantValueChanged === 'function') {
 				this.replicantValueChanged(...args);
 			}
         }
     },
     watch: {
-        replicantName: newVal => {
-            this.retargetReplicant(newVal);
+        replicantName(newVal) {
+            this.declareReplicant(newVal);
         },
-        replicantBundle: newVal=> {
-            this.retargetReplicant(this.replicantName, newVal);
-        }        
+        replicantBundle(newVal) {
+            this.declareReplicant(this.replicantName, newVal);
+        }     
     }
 });
